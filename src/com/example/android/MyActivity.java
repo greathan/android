775cef57a1eb98b6ef84face08d3e0314d776f01
Great Han <hwlyglove@gmail.com>
2012-12-05 23:15:01 +0800

    async task http request

--- src/com/example/android/MyActivity.java
+++ src/com/example/android/MyActivity.java
@@ -2,6 +2,12 @@ package com.example.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.AsyncTask;
+import android.net.http.AndroidHttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.HttpResponse;
+
+import java.io.IOException;
 
 public class MyActivity extends Activity {
     /**
@@ -10,6 +16,32 @@ public class MyActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+	    new NetworkTask().execute("http://www.google.com");
         setContentView(R.layout.main);
     }
+
+	private class NetworkTask extends AsyncTask<String, Void, HttpResponse> {
+		@Override
+		protected HttpResponse doInBackground(String... params) {
+			String link = params[0];
+			HttpGet request = new HttpGet(link);
+			AndroidHttpClient client = AndroidHttpClient.newInstance("Android");
+			try {
+				return client.execute(request);
+			} catch (IOException e) {
+				e.printStackTrace();
+				return null;
+			} finally {
+				client.close();
+			}
+		}
+
+		@Override
+		protected void onPostExecute(HttpResponse result) {
+			//Do something with result
+			if (result != null) {
+
+			}
+		}
+	}
 }
